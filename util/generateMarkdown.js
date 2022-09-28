949bb095c76841575cb8fc2fa85c24e555c462c6
Zac Smart <zac_chaz@outlook.com>
2022-09-28 23:36:35 +1000

    added license badge function and license link function, allmost complete

--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -1,10 +1,24 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license !== 'N/A') {
+        return` ![badge](https://img.shields.io/badge/license-${license}-blue)`;
+    } else {
+        return ' ';
+    }
+    }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license !== "N/A") {
+        return `\n * [license](#license)\n`
+    }
+    else{
+        return " ";
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
